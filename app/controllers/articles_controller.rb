67451a2a1bf0ab8ac372f166e86b96442a781793
takenokoya <takuya1002@gmail.com>
2019-03-11 10:49:21 +0900

    削除機能の追加とコントローラのリファクタリング

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -18,21 +18,25 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
     redirect_to @article
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path
   end
 
   private
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
 
+  def set_article
+    @article = Article.find(params[:id])
+  end
 end
